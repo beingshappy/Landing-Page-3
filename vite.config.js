13e2b6c2530003b0416b5d07686461df99274eaa
Sahasransu Satpathy <satpathy707@gmail.com>
2025-08-20 01:18:32 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/maac/', // 👈 yaha apne repo ka naam
+  base: '/maac/',   // 👈 yaha apne repo ka naam dalna
 })
