9ce1fd2079a4d8056876c130fbf3756519ef276b
Sahasransu Satpathy <satpathy707@gmail.com>
2025-08-20 01:54:52 +0530

    1

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/maac/',  // 👈 GitHub repo name
+  base: '/Landing-Page-3/',   // 👈 ye line add karo
 })
